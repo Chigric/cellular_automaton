40bc6e40664cc5ce203a8ddce8339f1b4fa0ecc1
Chigric <ggggg.writer.99@mail.ru>
2020-06-02 18:46:51 +0300

    fix func_scroll, overwrite Point -> Cell, code are more functionalshena

--- lifegame.py
+++ lifegame.py
@@ -15,40 +15,34 @@ def print_cells(cells):
 
 
 def func_scroll(delta_time: float, window: GraphWin, text: Text):
+    # Ticks (time)
     all_time = 0
-    loop_time = 0
     x_end = 10
     y_end = 10
     x_stat = 0
     y_stat = 0
 
-    def scroll(tiks):
+    def scroll(ticks):
         nonlocal x_stat, y_stat, x_end, y_end
         # Window
-        x_end += tiks
-        y_end += tiks
-        x_stat -= tiks
-        y_stat -= tiks
+        x_end += ticks
+        y_end += ticks
+        x_stat -= ticks
+        y_stat -= ticks
         # Text
-        text.move(tiks * 0.75, tiks * -0.9)
-        text.setText("Tiks: " + str(all_time))
-        print("All Tiks: " + str(all_time))
+        text.move(ticks * 0.8, ticks * -0.9)
+        text.setText("Ticks: " + str(all_time))
+        print("All Ticks: " + str(all_time))
         window.setCoords(x_stat, y_stat, x_end, y_end)
 
     def next_moment():
-        nonlocal all_time, loop_time
-        # Увеличение масштаба
-        if loop_time > 1.0:
-            # Time
-            loop_time = 0
+        nonlocal all_time
 
         # Scroll Window
         scroll(1)
-
         # Подсчёт времени
         all_time += delta_time
-        loop_time += delta_time
-
+        # Sleep
         time.sleep(0.5)
 
         return all_time
@@ -68,7 +62,7 @@ def draw_step(cells, window, time_scroll):
     return draw_step(step(cells), window, time_scroll)
 
 
-def get_neighbors(pt: Point):
+def get_neighbors(pt: Cell):
     return [Cell(Point(pt.getX() + i, pt.getY() + j))
             for i in range(-1, 2, 1) for j in range(-1, 2, 1) if j != 0 or i != 0]
 
@@ -80,15 +74,17 @@ def compare_points(left: Point, right: Point):
         else False
 
 
-def alive_rools(neighbors_quant):
+def alive_rools(neighbors_quant: int):
     return neighbors_quant == 3
 
 
-def live_rools(neighbors_quant):
+def live_rools(neighbors_quant: int):
     if neighbors_quant <= 1 or neighbors_quant > 3:
         return True
     elif neighbors_quant in [2, 3]:
         return False
+    else:
+        return None
 
 
 """
@@ -114,35 +110,26 @@ def live_rools(neighbors_quant):
 
 
 def step(cells: list):
-    def getCellsFromNeighbors(curPoint: Cell, neighbors: list):
-        cells_neighbors = []
-        not_cells_neighbors = []
-        for pt in cells:
-            if not pt == curPoint:
-                for neig in neighbors:
-                    if pt == neig:
-                        cells_neighbors.append(pt)
+    # Выбираем из соседей живые клетки
+    def getCellsFromNeighbors(cur_point: Cell, neighbors: list):
+        return [pt
+                for pt in cells if not pt == cur_point
+                for neig in neighbors if pt == neig]
 
-        return cells_neighbors
+    # Возращает кол-во соседей-клеток вокруг cur_point
+    def lookAtNeighbors(cur_point: Cell):
+        return len(getCellsFromNeighbors(cur_point, get_neighbors(cur_point)))
 
-    # Возращает кол-во соседей-клеток pt
-    def lookAtNeighbors(pt: Point):
-        neighbors = get_neighbors(pt)
-        neig_cells = getCellsFromNeighbors(pt, neighbors)
-        neighbors_quant = len(neig_cells)
-
-        return neighbors_quant
-
-    # Добавляет в other_neighbors всех соседей curPoint,
-    # которых не в cells и там не присутствуют
-    def new_other_neighbors(curPoint: Point, other_neighbors: list):
-        neighbors = get_neighbors(curPoint)
+    # Добавляет в other_neighbors всех соседей cur_pt,
+    # которых нет в cells и там не присутствуют
+    def new_other_neighbors(cur_pt: Cell, other_neighbors: list):
+        neighbors = get_neighbors(cur_pt)
         for i in neighbors:
             # Условие которое должно достигать (len(cells) - 1)
             # для подтверждения, что i нет в cells
             proof = 0
             for j in cells:
-                if not i == j and not j == curPoint:
+                if not i == j and not j == cur_pt:
                     proof += 1
                     # нужно выйти из цикла cells
                     continue
@@ -173,12 +160,12 @@ def step(cells: list):
 
         return will_revive_cells
 
-    newCells = cells.copy()
+    new_cells = cells.copy()
     other_neighbors = []
     for pt in cells:
         neighbors_quant = lookAtNeighbors(pt)
         if live_rools(neighbors_quant):
-            newCells.remove(pt)
+            new_cells.remove(pt)
 
         new_other_neighbors(pt, other_neighbors)
         will_revive_cells = revive_cells(other_neighbors)
@@ -186,41 +173,38 @@ def step(cells: list):
         # Проверка уникальности значений will_revive_cells относительно newCells
         for i in will_revive_cells:
             proof = 0
-            for j in newCells:
+            for j in new_cells:
                 if not i == j:
                     proof += 1
-            if proof == len(newCells):
-                newCells.append(i)
+            if proof == len(new_cells):
+                new_cells.append(i)
 
-    return newCells
+    return new_cells
 
 
-def drawText(win: GraphWin):
+def draw_text(win: GraphWin):
     score = 0
     txt = Text(Point(9, 0.5), "All time: " + str(score))
     txt.setOutline('white')
     txt.setFace('arial')
     txt.draw(win)
-
     return txt
 
 
-def drawMainWindow():
+def draw_main_window():
     mainWin = GraphWin(title="MINE", width=500, height=500) # create a window
     mainWin.setCoords(0, 0, 10, 10)  # set the coordinates of the window; bottom left is (0, 0) and top right is (10, 10)
     mainWin.setBackground(color_rgb(105, 100, 95))  # some gray color
-
     return mainWin
 
 
 if __name__ == '__main__':
     # Рисование окна
-    mainWin = drawMainWindow()
+    mainWin = draw_main_window()
     # Некий счётчк на экране
-    txt = drawText(mainWin)
+    txt = draw_text(mainWin)
     # Набор клеток
     list_cells = cell_factory([Point(4, 5), Point(5, 5), Point(6, 5), Point(5, 6)])
-
     draw_step(list_cells, mainWin, func_scroll(1, mainWin, txt))
 
     mainWin.getMouse()  # Pause before closing (to view result)
